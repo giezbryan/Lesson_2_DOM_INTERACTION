e31049d67baec51a754c79419d0e865b3f35cce0
unknown <bryanchua0724@gmail.com>
2021-10-20 19:35:52 +0800

    Activity 1 - Data

---
+++ Activity 1 - Data Binding Problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp ({
+    data () {
+        return {
+            Name : 'Mark Bryan A. Chua',
+            Age : 19,
+            url : "http://vuejs.org/images/logo.png"
+        }
+    }
+});
+
+app.mount('#assignment');
