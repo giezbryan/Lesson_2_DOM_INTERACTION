af5f5428684b3da870d3bbd4be2d8b6cd157adc8
unknown <bryanchua0724@gmail.com>
2021-10-20 22:03:40 +0800

    Activity 3 - Executing Buttons and Displaying Results

--- Activity 3 - Reactivity Problem/basics-assignment-3-problem/app.js
+++ Activity 3 - Reactivity Problem/basics-assignment-3-problem/app.js
@@ -15,9 +15,33 @@ const app = Vue.createApp ({
                 }, 5000)
             }
         }
-    }
+    },
 
+    methods: {
+        Plusses(num) {
+            this.counter = this.counter + num;
+        },
+        Plus(num) {
+            this.counter = this.counter + num;
+        },
+        Result() {
+            if (this.counter == 0)
+            {
+                return this.counter;
+            }
+            else if (this.counter > 0 && this.counter < 37)
+            {
+                return "Not there yet!";
+            }
+            else if (this.counter > 37 )
+            {
+                return "Too much!";
+            }
 
+            return this.counter;
+        },
+
+    },
 });
 
 app.mount('#assignment');
