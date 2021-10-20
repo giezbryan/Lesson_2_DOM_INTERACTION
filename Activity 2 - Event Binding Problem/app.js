9d9f7a80dead93d40cada2455bbe1bdc39b4af02
unknown <bryanchua0724@gmail.com>
2021-10-20 20:43:18 +0800

    Methods Event Binding

--- Activity 2 - Event Binding Problem/app.js
+++ Activity 2 - Event Binding Problem/app.js
@@ -4,7 +4,18 @@ const app = Vue.createApp ({
             Result1: "",
             Result2: ""
         }
+    },
+    methods: {
+        DisplayAlert() {
+            alert('Alert! Alert! Alert!')
+        },
+        SetOutput1(event) {
+            this.Result1 = event.target.value
+        },
+        SetOutput2(event) {
+            this.Result2 = event.target.value
+        }
     }
-})
+});
 
 app.mount('#assignment');
