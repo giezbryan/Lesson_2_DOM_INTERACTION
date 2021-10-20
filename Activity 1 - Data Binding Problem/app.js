80bceb371963f0a15e66f76671addc332ea5502f
unknown <bryanchua0724@gmail.com>
2021-10-20 19:51:09 +0800

    Activity 1 - Methods with Math Random

--- Activity 1 - Data Binding Problem/app.js
+++ Activity 1 - Data Binding Problem/app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp ({
     data () {
         return {
             Name : 'Mark Bryan A. Chua',
-            Age : 19,
+            Age : 20,
             url : "http://vuejs.org/images/logo.png"
+        };
+    },
+    methods: {
+        PlusAge() {
+            return this.Age + 5;
+        },
+        FavoriteNumber() {
+            const RandomNumber = Math.random();
+            if (RandomNumber < 0.5)
+            {
+                return "0";
+            }
+            else
+            {
+                return "1";
+            }
         }
     }
 });
